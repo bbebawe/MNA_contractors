f8647c072912d655d3bffea87d00f50ede2c30aa
MBezzubik <m.bezzubik@gmail.com>
2020-06-30 18:19:46 +0100

    Navbar transition added

--- js/index.js
+++ js/index.js
@@ -2,13 +2,23 @@ $(window).on('scroll', function () {
     /* checks current scroll position */
     let y_scroll_pos = window.pageYOffset;
     /* checks the height of the browser */
-    let scroll_pos_test = 51;
+    let scroll_pos_test = 0;
     let logo = document.getElementById("logo");
+    let navbar = document.getElementById('navbar');
 
-    /* if splashscreen is visible, use fixed, else use scroll */
     if (y_scroll_pos > scroll_pos_test) {
-        logo.style.display = "none";
+        navbar.classList.add("navbar-transition");
+        document.getElementById("logo").style.height = "50px";
+        document.getElementById("logo").style.width = "75px";
+        
+        let x = document.getElementsByClassName("divider");
+        for (i = 0; i < x.length; i++) {
+            x[i].style.height = '5px';
+        }
+
     } else {
-        logo.style.display = "inline-block";
+        navbar.classList.remove("navbar-transition");
+        document.getElementById("logo").style.height = "66px";
+        document.getElementById("logo").style.width = "100px";
     }
 });
